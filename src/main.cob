5b1eb91fbba96eb37bfeaee7fc958fd284342ebb
Garret Engh <engh@usf.edu>
2025-09-08 23:19:08 +0000

    Added full password validation including special characters and all that. Added function that will display the prompts as well as save them to the output file. See comments above the function displayAndWrite for implementation.

--- src/main.cob
+++ src/main.cob
@@ -25,42 +25,56 @@
            01 accountRecord PIC X(100).
 
          WORKING-STORAGE SECTION.
+           *>Variables for user input and output
            01 newUser        PIC X.
            01 userName       PIC X(30).
            01 userPassword   PIC X(30).
-           01 passwordValid  PIC X VALUE "N".
+           01 messageVar     PIC X(100).
 
-           01 accountCount PIC 9 VALUE 0.
-           01 endOfFile PIC X VALUE "N".
+           *>Password validation variables
+           01 passwordValid    PIC X VALUE "N".
+           01 i                PIC 99 VALUE 1.
+           01 upperFound       PIC X VALUE "N".
+           01 digitFound       PIC X VALUE "N".
+           01 specialFound     PIC X VALUE "N".
+           01 char             PIC X.
+
+           *>Account counting and verification variables
+           01 accountCount    PIC 9 VALUE 0.
+           01 endOfFile       PIC X VALUE "N".
 
 
        PROCEDURE DIVISION.
            OPEN INPUT userInputFile
-           OPEN OUTPUT userOutputFile
+           OPEN OUTPUT userOutputFile *>Overwrites existing output file , if any. Delete to append instead.
            READ userInputFile INTO newUser.
            READ userInputFile INTO userName.
            READ userInputFile INTO userPassword.
 
 
            PERFORM countAccounts.
-           OPEN EXTEND accountFile *> Truncates instead of overwriting.
+           OPEN EXTEND accountFile *> appends instead of overwriting.
 
-           DISPLAY "Welcome to inCollege by team Wyoming!".
-           DISPLAY "Are you a new user? (Y/N)".
+           Move "Welcome to inCollege by team Wyoming!" TO messageVar
+           PERFORM displayAndWrite.
+           MOVE "Are you a new user? (Y/N)" TO messageVar
+           PERFORM displayAndWrite.
 
-           DISPLAY accountCount " accounts currently exist.".
-           IF (newUser = "Y" OR newUser = "y") AND accountCount >= 5
-                DISPLAY "Maximum number of accounts reached. Cannot register new users."
-                CLOSE userInputFile
-                CLOSE userOutputFile
-                CLOSE accountFile
-                STOP RUN
+
+           DISPLAY accountCount " accounts currently exist.".*>Debug, can be removed
+           IF newUser = "Y" OR newUser = "y"
+               IF accountCount >= 5
+                   DISPLAY "Maximum number of accounts reached. Cannot register new users."
+                   CLOSE userInputFile
+                   CLOSE userOutputFile
+                   CLOSE accountFile
+                   STOP RUN
                ELSE
-                   IF newUser = "Y" OR newUser = "y"
-                       PERFORM newUserRegistration
+                   PERFORM newUserRegistration
+               END-IF
            ELSE
                PERFORM existingUserLogin
-           END-IF.
+           END-IF
 
            DISPLAY "Thank you for using inCollege".
            CLOSE userInputFile.
@@ -80,23 +94,47 @@
                        DISPLAY "Account Created."
 
                    ELSE
-                       DISPLAY "Please try again. Password must be 8 or 12 characters long.".
+                       MOVE "Please try again. Password must be 8 or 12 characters long." TO messageVar
+                       PERFORM displayAndWrite.
+                   END-IF.
                EXIT.
 
            *> Existing user login process (NEEDS TO BE IMPLEMENTED)
            existingUserLogin.
-               DISPLAY "Please enter your login credentials.".
-               DISPLAY "Login process completed.".
+               MOVE "Please enter your login credentials." TO messageVar
+               PERFORM displayAndWrite.
+               MOVE "Login process completed." TO messageVar
+               PERFORM displayAndWrite.
                EXIT.
 
 
            *> Validate password length
            validatePassword.
-                IF FUNCTION LENGTH(FUNCTION TRIM(userPassword TRAILING))  = 8 OR FUNCTION LENGTH(FUNCTION TRIM(userPassword TRAILING)) = 12
+                IF FUNCTION LENGTH(FUNCTION TRIM(userPassword TRAILING))  > 8 AND FUNCTION LENGTH(FUNCTION TRIM(userPassword TRAILING)) < 12
                     MOVE "Y" TO passwordValid
                 ELSE
-                    DISPLAY "Invalid password length: " FUNCTION LENGTH(userPassword)
+                    MOVE "Invalid password length: " FUNCTION LENGTH(FUNCTION TRIM(userPassword TRAILING)) ". Password must be between 8 and 12 characters." TO messageVar
+                    PERFORM displayAndWrite
                 END-IF.
+
+               PERFORM Varying i FROM 1 BY 1 UNTIL i > FUNCTION LENGTH(FUNCTION TRIM(userPassword TRAILING))
+                   MOVE userPassword(i:1) TO char
+                   IF char >= "A" AND char <= "Z"
+                       MOVE "Y" TO upperFound
+                   END-IF
+                   IF char >= "0" AND char <= "9"
+                       MOVE "Y" TO digitFound
+                   END-IF
+                   IF char = "!" OR char = "@" OR char = "#" OR char = "$" OR char = "%" OR char = "^" OR char = "&" OR char = "*"
+                       MOVE "Y" TO specialFound
+                   END-IF
+                END-PERFORM.
+                   IF upperFound = "Y" AND digitFound = "Y" AND specialFound = "Y"
+                          MOVE "Y" TO passwordValid
+                   ELSE
+                       MOVE "Password must contain at least one uppercase letter, one special character, and one digit." TO messageVar
+                       PERFORM displayAndWrite
+                   END-IF.
            EXIT.
 
            *> Count existing accounts
@@ -114,3 +152,12 @@
                END-PERFORM.
                CLOSE accountFile.
            EXIT.
+
+           *> Display message and write to output file, needed for every user prompt
+           *> MOVE "message" to messageVar
+           *> displayAndWrite.
+           displayAndWrite.
+               DISPLAY messageVar
+               MOVE messageVar TO userOutputRecord
+               WRITE userOutputRecord
+               EXIT.
